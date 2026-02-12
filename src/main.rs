d869d1979a365e7c3c195a89d0ada60035d27e97
wnw231423 <2684722187@qq.com>
2026-02-12 19:26:35 +0800

    refactor: apply clap crate for CLI

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,56 @@
+use clap::{Parser, Subcommand};
 use std::{
-    env,
-    fs::File,
+    fs::File, path::PathBuf,
 };
 
 mod core;
 mod huffman;
 mod service;
 
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// compress a file
+    Compress {
+        /// The source file that you want to compress.
+        src: PathBuf,
+        /// The dest file path to store compressed file
+        dest: PathBuf,
+    },
+    /// extract a ruf-compressed file
+    Extract {
+        /// The source file that you want to extract
+        src: PathBuf,
+        /// The dest file path to store extracted file
+        dest: PathBuf,
+    }
+}
+
 fn main() {
-    let args: Vec<_> = env::args().collect();
-    let op = &args[1];
-    let src = &args[2];
-    let dest = &args[3];
-    let src_f = File::open(src).unwrap();
-    let mut dest_f = File::create_new(dest).unwrap();
-    match op.as_str() {
-        "compress" => {
+    let args = Args::parse();
+
+    match args.command {
+        Commands::Compress { 
+            src, 
+            dest 
+        } => {
+            let src_f = File::open(src).unwrap();
+            let mut dest_f = File::create_new(dest).unwrap();
             service::compress_file(&src_f, &mut dest_f);
-        }
-        "extract" => {
+        },
+        Commands::Extract { 
+            src, 
+            dest 
+        } => {
+            let src_f = File::open(src).unwrap();
+            let mut dest_f = File::create_new(dest).unwrap();
             service::extract_file(&src_f, &mut dest_f);
-        }
-        _ => {
-            panic!("unknown operation.");
         }
     }
 }
